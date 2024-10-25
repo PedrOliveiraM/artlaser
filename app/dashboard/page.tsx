686fb0450a31a4c7217bd2f49cb28d3e1603b0dc
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-25 18:00:51 -0300

    refactor: include toast errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,37 +6,55 @@ import { useEffect, useState } from 'react'
 import { ProductsTable } from './_components/Products-table'
 import { BannersTable } from './_components/Banners-table'
 import DashboardLayout from './dashboardLayout'
+import { useToast } from '@/hooks/use-toast' // Importando o useToast
 
 export default function DashBoard() {
   const [products, setProducts] = useState<Product[]>([])
   const [banners, setBanners] = useState<Banner[]>([])
   const [filter, setFilter] = useState('')
+  const { toast } = useToast() // Inicializando o toast
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const res = await fetch('/api/products')
-      if (res.ok) {
+      try {
+        const res = await fetch('/api/products')
+        if (!res.ok) {
+          throw new Error('Falha ao buscar produtos')
+        }
         const data = await res.json()
         setProducts(data)
-      } else {
-        console.error('Failed to fetch products')
+      } catch (error) {
+        console.error(error)
+        toast({
+          title: 'Erro',
+          description: 'Não foi possível listar os produtos.',
+          variant: 'destructive',
+        })
       }
     }
     fetchProducts()
-  }, [])
+  }, [toast]) // Adicionando toast como dependência
 
   useEffect(() => {
     const fetchBanners = async () => {
-      const res = await fetch('/api/banners')
-      if (res.ok) {
+      try {
+        const res = await fetch('/api/banners')
+        if (!res.ok) {
+          throw new Error('Falha ao buscar banners')
+        }
         const data = await res.json()
         setBanners(data)
-      } else {
-        console.error('Failed to fetch banners')
+      } catch (error) {
+        console.error(error)
+        toast({
+          title: 'Erro',
+          description: 'Não foi possível listar os banners.',
+          variant: 'destructive',
+        })
       }
     }
     fetchBanners()
-  }, [])
+  }, [toast]) // Adicionando toast como dependência
 
   return (
     <DashboardLayout onFilterChange={setFilter}>
