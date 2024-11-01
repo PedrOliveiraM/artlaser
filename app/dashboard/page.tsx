c3ff335aafec86d46c7924be83597a240dea09ef
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-01 14:53:56 -0300

    refactor: remove unused components related to banners and products

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,57 +1,78 @@
-// page.tsx
 'use client'
-
 import { Banner, Product } from '@prisma/client'
 import { useEffect, useState } from 'react'
-import { ProductsTable } from './_components/Products-table'
-import { BannersTable } from './_components/Banners-table'
-import DashboardLayout from './dashboardLayout'
+import Loading from '@/components/loading'
+import { useToast } from '@/hooks/use-toast'
+import { DataTable } from './_components/DataTable'
+import { Productcolumns } from './_components/Product-columns'
+import { BannerColumns } from './_components/Banner-Columns'
 
-export default function DashBoard() {
-  const [products, setProducts] = useState<Product[]>([])
-  const [banners, setBanners] = useState<Banner[]>([])
-  const [filter, setFilter] = useState('')
+export default function Home() {
+  const [dataProducts, setDataProducts] = useState<Product[]>([])
+  const [dataBanners, setDataBanners] = useState<Banner[]>([])
+  const [loading, setLoading] = useState(true)
+  const { toast } = useToast()
+
+  const fetchDataProducts = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('/api/products')
+      const result = await response.json()
+      setDataProducts(result)
+      setLoading(false)
+      toast({
+        variant: 'success',
+        title: 'Sucesso',
+        description: 'Produtos carregados com sucesso.',
+      })
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: 'There was a problem with your request.',
+      })
+    }
+  }
+  const fetchDataBanners = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('/api/banners')
+      const result = await response.json()
+      setDataBanners(result)
+      setLoading(false)
+      toast({
+        variant: 'success',
+        title: 'Sucesso',
+        description: 'Banners carregados com sucesso.',
+      })
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: 'There was a problem with your request.',
+      })
+    }
+  }
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch('/api/products')
-      if (res.ok) {
-        const data = await res.json()
-        setProducts(data)
-      } else {
-        console.error('Failed to fetch products')
-      }
-    }
-    fetchProducts()
+    fetchDataProducts()
+    fetchDataBanners()
   }, [])
 
-  useEffect(() => {
-    const fetchBanners = async () => {
-      const res = await fetch('/api/banners')
-      if (res.ok) {
-        const data = await res.json()
-        setBanners(data)
-      } else {
-        console.error('Failed to fetch banners')
-      }
-    }
-    fetchBanners()
-  }, [])
-
+  if (loading) return <Loading />
   return (
-    <DashboardLayout onFilterChange={setFilter}>
-      <div className="space-y-2">
-        <ProductsTable
-          products={products}
-          totalProducts={products.length}
-          filter={filter}
-        />
-        <BannersTable
-          banners={banners}
-          totalBanners={banners.length}
-          filter={filter}
-        />
-      </div>
-    </DashboardLayout>
+    <div className="container mx-auto w-3/4 p-5 md:w-full">
+      <DataTable
+        columns={Productcolumns(fetchDataProducts)}
+        data={dataProducts}
+        title="Produtos"
+      />
+
+      <DataTable
+        columns={BannerColumns(fetchDataBanners)}
+        data={dataBanners}
+        title="Banners"
+      />
+    </div>
   )
 }
