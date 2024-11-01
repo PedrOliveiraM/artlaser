ee51b1684ef822fd6099f6ec1d27136cf3992610
PedrOliveiraM <pedropucmont@gmail.com>
2024-10-31 22:06:32 -0300

    feat: add loading component with circular progress indicator

--- .history/components/loading_20241031215501.tsx
+++ .history/components/loading_20241031215501.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import CircularProgress from '@mui/material/CircularProgress'
 import Box from '@mui/material/Box'
 
-export default function Loading() {
+export default function CircularIndeterminate() {
   return (
     <Box
       sx={{
