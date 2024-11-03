68a2071a859ddeb8663723846764b1eb95668a56
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-02 22:42:28 -0300

    refactor: remove onFilterChange prop from DashboardLayout and simplify structure

--- app/dashboard/dashboardLayout.tsx
+++ app/dashboard/dashboardLayout.tsx
@@ -6,20 +6,14 @@ import Footer from './_components/Footer'
 
 interface LayoutPageProps {
   children: React.ReactNode
-  onFilterChange: (filter: string) => void
 }
 
-export default function DashboardLayout({
-  children,
-  onFilterChange,
-}: LayoutPageProps) {
+export default function DashboardLayout({ children }: LayoutPageProps) {
   return (
     <main className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 sm:px-6 sm:py-4">
-        <Header onFilterChange={onFilterChange} />
-        <main className="grid flex-1 items-start gap-2 bg-muted/40 p-4 sm:px-6 sm:py-0 md:gap-4">
-          {children}
-        </main>
+      <div className="flex flex-col px-10">
+        <Header />
+        <main className="w-full">{children}</main>
       </div>
       <Footer />
     </main>
