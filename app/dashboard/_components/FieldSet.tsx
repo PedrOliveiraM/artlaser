52c3b701da63bdaaa3dce6d0139f46acfbb8838e
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-02 22:17:48 -0300

    feat: add FieldSet component for structured content display

---
+++ app/dashboard/_components/FieldSet.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+interface IFieldSetProps {
+  children: React.ReactNode
+  title: string
+  description: string
+}
+
+export default function FieldSet({
+  children,
+  title,
+  description,
+}: IFieldSetProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  )
+}
