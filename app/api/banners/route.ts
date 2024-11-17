457920e17ac13beb3e43718835bf4febcea0d30d
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-17 15:20:03 -0300

    feat: create route api post banner

--- app/api/banners/route.ts
+++ app/api/banners/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest, NextResponse } from 'next/server'
 import { db } from '@/lib/prisma'
 
 export async function GET() {
@@ -5,4 +6,22 @@ export async function GET() {
   return Response.json(banners, {
     status: 200,
   })
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json()
+    console.log('DATA RECEBIDA : ', body)
+
+    const createdBanner = await db.banner.create({
+      data: body,
+    })
+
+    if (!createdBanner) throw new Error('Erro ao criar produto!')
+
+    return Response.json({ createdBanner }, { status: 201 })
+  } catch (error) {
+    console.error('Erro ao processar requisição:', error)
+    return NextResponse.json({ message: 'Erro interno do servidor' }, { status: 500 })
+  }
 }
