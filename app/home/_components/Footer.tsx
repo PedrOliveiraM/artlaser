829a07f16c2e1d5eb9245d3d1612b46fc43a166a
PedrOliveiraM <pedropucmont@gmail.com>
2024-12-17 11:31:46 -0300

    feat: update Footer component with social media links and new layout

--- app/home/_components/Footer.tsx
+++ app/home/_components/Footer.tsx
@@ -1,18 +1,57 @@
+import { IoLogoWhatsapp } from 'react-icons/io'
+import { AiFillInstagram } from 'react-icons/ai'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
 export function Footer() {
   return (
-    <footer className="border-t bg-background">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Artlaser © 2024
-          </p>
+    <footer className="bg-orange-400 text-black">
+      <div className="container mx-auto px-4 py-8">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* Logo e descrição */}
+          <div className="flex flex-col justify-center items-center">
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+              <Image
+                src="/imagens/Logo.png"
+                alt="Logo ArtLaser"
+                width={160}
+                height={60}
+              />
+            </Link>
+            <p className="text-base text-center">
+              Transformando ideias em brindes e artes personalizadas
+            </p>
+          </div>
+
+          {/* Redes sociais */}
+          <div>
+            <h3 className="text-lg font-semibold mb-4 text-center">Nossos Contatos</h3>
+            <div className="flex space-x-4 justify-center items-center">
+              <Link
+                href="https://api.whatsapp.com/send?phone=5577988438467"
+                className="flex gap-2 items-center"
+              >
+                <IoLogoWhatsapp size={24} />
+                +55 77 98843-8467
+              </Link>
+
+              <Link
+                href="https://www.instagram.com/artlaser_crr/"
+                className="flex gap-2 items-center"
+              >
+                <AiFillInstagram size={24} strokeWidth={2} />
+                @Artlaser_crr
+              </Link>
+            </div>
+          </div>
         </div>
-        <p className="text-center text-sm text-muted-foreground md:text-left">
-          Rua do mercado novo, 470, Correntina-BA
-        </p>
-        <p className="text-center text-sm text-muted-foreground md:text-left">
-          Desenvolvido por Pedro Oliveira
-        </p>
+
+        {/* Rodapé */}
+        <div className="mt-8 pt-4 border-t border-orange-200 text-center text-sm">
+          <p>&copy; 2024 Artlaser. Todos os direitos reservados</p>
+        </div>
       </div>
     </footer>
   )
